5eac3527a40c831c6c96767b020983992d56b1d8
purnima-poo <np03a190173@herladcollege.edu.np>
2022-06-17 15:03:59 +0545

    style: ui of homePage

--- covid_api/lib/view/pages/home_page.dart
+++ covid_api/lib/view/pages/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:covid_api/api_service/all_case_api.dart';
+import 'package:covid_api/provider/all_case_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,7 +13,49 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    Future.microtask(() {
+      Provider.of<AllCaseProvider>(context, listen: false).getAllCaseData();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Covid case"),
+      ),
+      body: Consumer<AllCaseProvider>(
+        builder: (context, child, value) {
+          return Column(
+            children: [
+              PieChart(
+                dataMap: {
+                  "active": allCases!.active!.toDouble(),
+                  "deaths": allCases!.deaths!.toDouble(),
+                  "recovered": allCases!.recovered!.toDouble()
+                },
+                legendOptions: const LegendOptions(
+                  legendPosition: LegendPosition.left,
+                ),
+                colorList: [
+                  Colors.pink.shade100,
+                  Colors.blue.shade100,
+                  Colors.lime,
+                ],
+                chartValuesOptions: const ChartValuesOptions(
+                    showChartValues: false, showChartValuesOutside: true),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {}, child: const Text("See countries"))
+            ],
+          );
+        },
+      ),
+    );
   }
 }
