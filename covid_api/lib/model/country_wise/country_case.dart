6ee9d3c2b44bef6e561fc989d59e198f2e1c197e
purnima-poo <np03a190173@herladcollege.edu.np>
2022-06-17 11:41:19 +0545

    added model of country case

---
+++ covid_api/lib/model/country_wise/country_case.dart
@@ -0,0 +1,18 @@
+class Country {
+  final String? country;
+  final TimeLine? timeline;
+  const Country({required this.country, required this.timeline});
+
+  factory Country.fromJson(Map<String, dynamic> json) {
+    return Country(country: json["country"], timeline: json["timeline"]);
+  }
+}
+
+class TimeLine {
+  final num? date;
+  const TimeLine({required this.date});
+
+  factory TimeLine.fromJson(Map<String, dynamic> json) {
+    return TimeLine(date: json['6/17/22']);
+  }
+}
